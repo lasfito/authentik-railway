21d2aa56e1337d05b2e187253e50e422de236e03
Wessel Valkenburg <wessel@prevue.ch>
2024-05-08 18:39:45 +0200

    feat: prerun with custom css.

---
+++ services/authentik-server/prerun.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DEF_ACCENT="#FF0090"
+DEF_LINK="#B900FF"
+ACCENT="${OVERRIDE_ACCENT_COLOUR:-${DEF_ACCENT}}"
+LINK="${OVERRIDE_LINK_COLOUR:-${DEF_LINK}}"
+
+cat > /web/dist/custom.css <<EOF
+:root {
+    --ak-accent: ${ACCENT};
+    --ak-dark-foreground-link: ${LINK};
+    --pf-c-nav__link--m-current--after--BorderColor: var(--ak-accent);
+    --pf-c-button--m-primary--BackgroundColor: var(--ak-dark-foreground-link);
+}
+
+.ak-login-container .ak-brand img {
+    max-width: 100%;
+}
+EOF
+
