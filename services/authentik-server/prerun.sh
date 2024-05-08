d4d2f94b2bdef28bbe21913a7d67df8258a7b21b
Wessel Valkenburg <wessel@prevue.ch>
2024-05-08 23:36:07 +0200

    feat: set custom css at every restart.

--- services/authentik-server/prerun.sh
+++ services/authentik-server/prerun.sh
@@ -8,18 +8,6 @@ LINK="${OVERRIDE_LINK_COLOUR:-${DEF_LINK}}"
 mkdir -p /web/dist
 chmod 777 /web/dist
 
-cat > /web/dist/custom.css <<EOF
-:root {
-    --ak-accent: ${ACCENT};
-    --ak-dark-foreground-link: ${LINK};
-    --pf-c-nav__link--m-current--after--BorderColor: var(--ak-accent);
-    --pf-c-button--m-primary--BackgroundColor: var(--ak-dark-foreground-link);
-}
-
-.ak-login-container .ak-brand img {
-    max-width: 100%;
-}
-EOF
-
-chmod 644 /web/dist/custom.css
+touch  /web/dist/custom.css 
+chmod 666 /web/dist/custom.css
 
