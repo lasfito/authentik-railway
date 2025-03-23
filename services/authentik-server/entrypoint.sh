d80e9dea99b95d3c9b03ea800a84b5b2bf779a39
lasfito <79036345+lasfito@users.noreply.github.com>
2025-03-24 00:24:28 +0900

    customization

--- services/authentik-server/entrypoint.sh
+++ services/authentik-server/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-DEF_ACCENT="#FF0090"
-DEF_LINK="#B900FF"
+DEF_ACCENT="#f4810b"
+DEF_LINK="#f4810b"
 ACCENT="${OVERRIDE_ACCENT_COLOUR:-${DEF_ACCENT}}"
 LINK="${OVERRIDE_LINK_COLOUR:-${DEF_LINK}}"
 
@@ -16,6 +16,47 @@ cat > /web/dist/custom.css <<EOF
 
 .ak-login-container .ak-brand img {
     max-width: 100%;
+}
+
+.pf-c-list pf-m-inline  {
+display:none
+}
+
+
+.pf-c-login__main-header pf-c-brand ak-brand { 
+    display: none;
+}
+
+.pf-c-background-image  {
+    display: none;
+}
+
+
+.pf-c-page__drawer {
+    background: white;
+}
+
+.pf-c-login__main {
+    background: white;
+}
+
+
+.pf-c-title {
+    visibility: hidden;
+}
+
+.pf-c-title:after {
+    content:'goodbye'; 
+    visibility: visible;
+    display: block;
+    position: absolute;
+    top:0;
+    left:0;
+}
+
+.pf-c-form-control {
+    background: white;
+    color: black;
 }
 EOF
 
